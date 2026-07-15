40a0e94a02da294e1f1a1064ddf527265de7a223
deltadasher <208410291+deltadasher@users.noreply.github.com>
2026-07-15 04:33:04 -0700

    Give player bios first-person personality

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pkl-ranked-v10';
+const CACHE = 'pkl-ranked-v11';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', (event) => {

5f41cc6809f9d1b914bd53cac6975dfb77b4cd8c
deltadasher <208410291+deltadasher@users.noreply.github.com>
2026-07-14 22:10:01 -0700

    Model runner gaps and catcher follows

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pkl-ranked-v2';
+const CACHE = 'pkl-ranked-v3';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', (event) => {
